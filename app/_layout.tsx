af88968f8c2950af5eafa222419266efc61502c0
Petro <petrotymchuck@gmail.com>
2026-01-25 08:17:03 +0200

    done design on first page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,18 @@
 import { Stack } from "expo-router";
+import { StatusBar, View } from "react-native";
 import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+      <View style={{ flex: 1, backgroundColor: "#18181B" }}>
+        {/* Задаємо статусбар темним */}
+        <StatusBar
+            barStyle="light-content"
+            backgroundColor="#18181B"
+        />
+
+        {/* Stack з усіма екранами */}
+        <Stack screenOptions={{ headerShown: false }} />
+      </View>
+  );
 }
