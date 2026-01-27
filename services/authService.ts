62ac585700742eeb65b7bee3ff6e984cf08027ba
Petro <petrotymchuck@gmail.com>
2026-01-27 20:53:02 +0200

    add base redux

---
+++ services/authService.ts
@@ -0,0 +1,39 @@
+import {createApi} from "@reduxjs/toolkit/query/react";
+import {IAuthResponse} from "@/types/auth/IAuthResponse";
+import {IRegister} from "@/types/auth/IRegister";
+import {createBaseQuery} from "@/utils/CreateBaaseQuery";
+import {ILogin} from "@/types/auth/ILogin";
+
+export const authService = createApi({
+    reducerPath: 'api/account',
+    baseQuery: createBaseQuery('Account'),
+    tagTypes: ['Account', 'AccountPassword'],
+    endpoints: (builder) => ({
+        login: builder.mutation<IAuthResponse, ILogin>({
+            query: (credentials) => ({
+                url: 'login',
+                method: 'POST',
+                body: credentials
+            })
+        }),
+
+
+        register: builder.mutation<IAuthResponse, IRegister>({
+            query: (credentials) => {
+                const formData =  null; //serialize(credentials);
+
+                return {
+                    url: 'register',
+                    method: 'POST',
+                    body: formData
+                };
+            }
+        })
+
+    })
+});
+
+export const {
+    useLoginMutation,
+    useRegisterMutation
+} = authService;
