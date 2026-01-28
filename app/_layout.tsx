8803e717067a0663c72a79a23297e0436786f5eb
Petro <petrotymchuck@gmail.com>
2026-01-28 07:04:57 +0200

    refactor layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,12 @@
 import { Stack } from "expo-router";
 import "../global.css";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import {useSystemBars} from "@/hooks/useSystemBars";
-import {StatusBar, useColorScheme} from "react-native";
 
 export default function RootLayout() {
-    const isDark = useColorScheme() === "dark";
-
-    useSystemBars();
 
     return (
         <>
             <SafeAreaProvider>
-                <StatusBar barStyle={isDark ? "light-content" : "dark-content"} />
                 <Stack screenOptions={{ headerShown: false }} />
             </SafeAreaProvider>
         </>
