0d4532bc205d4d2edacc8638b17b4aeaa6ef8a55
Petro <petrotymchuck@gmail.com>
2026-01-29 18:38:25 +0200

    fix createBaseQuery.ts

--- constants/Urls.ts
+++ constants/Urls.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = 'http://192.168.0.107:5240';
+export const BASE_URL = 'http://192.168.0.101:5240';
--- services/authService.ts
+++ services/authService.ts
@@ -1,7 +1,7 @@
 import {createApi} from "@reduxjs/toolkit/query/react";
 import {IAuthResponse} from "@/types/auth/IAuthResponse";
 import {IRegister} from "@/types/auth/IRegister";
-import {createBaseQuery} from "@/utils/CreateBaaseQuery";
+import {createBaseQuery} from "@/utils/createBaseQuery";
 import {ILogin} from "@/types/auth/ILogin";
 import {serialize} from "object-to-formdata";
 
