207a3b09514d909cf4ed71c6e657ef92fb12cc86
Petro <petrotymchuck@gmail.com>
2026-02-10 20:18:54 +0200

    add safe area to layout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,18 +1,21 @@
-import { View, KeyboardAvoidingView, Platform } from "react-native";
-import { Slot } from "expo-router";
-import { AuthTabs } from "../../components/auth/AuthTabs";
+import {View, KeyboardAvoidingView, Platform} from "react-native";
+import {Slot} from "expo-router";
+import {AuthTabs} from "../../components/auth/AuthTabs";
+import {SafeAreaView} from "react-native-safe-area-context";
 
 export default function AuthLayout() {
     return (
         <View className="flex-1 bg-white dark:bg-zinc-950">
-            <KeyboardAvoidingView
-                behavior={Platform.OS === "ios" ? "padding" : undefined}
-                className="flex-1"
-            >
-                <Slot />
-            </KeyboardAvoidingView>
+            <SafeAreaView className="flex-1 p-6">
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === "ios" ? "padding" : undefined}
+                    className="flex-1"
+                >
+                    <Slot/>
+                </KeyboardAvoidingView>
 
-            <AuthTabs />
+                <AuthTabs/>
+            </SafeAreaView>
         </View>
     );
 }
